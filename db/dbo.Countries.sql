c64b6652d7ab7a55ef02fb1bdcc5c2bb018b8d6b
Luiz Flávio Felizardo <lfvarginha@gmail.com>
2013-02-18 16:39:30 -0300

    Dropdown menu

--- db/dbo.Countries.sql
+++ db/dbo.Countries.sql
@@ -2,7 +2,7 @@
     [IdCountry]   INT        NOT NULL,
     [Name]        NCHAR (40) NOT NULL,
     [IdContinent] INT        NOT NULL,
-    PRIMARY KEY CLUSTERED ([IdCountry] ASC), 
-    CONSTRAINT [FK_Countries_ToContinents] FOREIGN KEY ([IdContinent]) REFERENCES [dbo].[Continents]([IdContinent])
+    PRIMARY KEY CLUSTERED ([IdCountry] ASC),
+    CONSTRAINT [FK_Countries_ToContinents] FOREIGN KEY ([IdContinent]) REFERENCES [dbo].[Continents] ([IdContinent])
 );
 
