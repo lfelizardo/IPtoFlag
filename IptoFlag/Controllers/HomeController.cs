2b0a56456057828bf0e9a9ffb5ee44779b9a701f
Luiz Flávio Felizardo <lfvarginha@gmail.com>
2013-02-19 01:50:15 -0300

    KendoUI fixes other bugFixes

--- IptoFlag/Controllers/HomeController.cs
+++ IptoFlag/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace IptoFlag.Controllers
 {
@@ -33,5 +36,18 @@ namespace IptoFlag.Controllers
             ViewBag.Countries = this.repository.GetCountries().ToList();
             return View();
         }
+
+
+        public ActionResult ReturnInfos([DataSourceRequest]DataSourceRequest request)
+        {
+            return Json(this.repository.GetInfos().Select(x => new {IdInfo = x.IdInfo, 
+                                                                    IdCountry = x.IdCountry, 
+                                                                    InfoCountry = x.InfoCountry, 
+                                                                    IP = x.IP, 
+                                                                    Location = x.Location, 
+                                                                    Date = x.Date }).ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
